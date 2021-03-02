30955c2d80f22e3e3b2e0cff7c8f75f7e40726f6
Francisco Rafael <francisco_rafael@hotmail.com.br>
2021-03-02 19:20:12 -0300

    Data Context

---
+++ Shop/Data/DataContext.cs
@@ -0,0 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using Shop.Models;
+
+namespace Backoffice.Data
+{
+    public class DataContext : DbContext //herda de DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options)
+            : base(options)
+        {
+        }
+
+        //Mapeamento atraves dos modelos
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
