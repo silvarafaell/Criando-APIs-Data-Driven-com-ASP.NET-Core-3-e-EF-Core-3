a9350859ec85d04efa967246a1ff88851e7c2b90
Francisco Rafael <francisco_rafael@hotmail.com.br>
2021-03-02 18:50:21 -0300

    Validando o modelo

--- Shop/Controllers/CategoryController.cs
+++ Shop/Controllers/CategoryController.cs
@@ -28,6 +28,10 @@ public class CategoryController : ControllerBase
     [Route("")]  //chega no metodo
     public async Task<ActionResult<List<Category>>> Post([FromBody] Category model)
     {
+        // Verifica se os dados são válidos
+        if (!ModelState.IsValid)
+            return BadRequest(ModelState);
+
         return Ok(model);
     }
 
@@ -35,10 +39,15 @@ public class CategoryController : ControllerBase
     [Route("{ind:int}")]  //chega no metodo
     public async Task<ActionResult<List<Category>>> Put(int id, [FromBody] Category model)
     {
-        if (model.Id == id)
-            return Ok(model);
+        // Verifica se o ID informado é o mesmo do modelo
+        if (id != model.Id)
+            return NotFound(new { message = "Categoria não encontrada" });
 
-        return NotFound();
+        // Verifica se os dados são válidos
+        if (!ModelState.IsValid)
+            return BadRequest(ModelState);
+
+        return Ok(model);
     }
 
     [HttpDelete]
