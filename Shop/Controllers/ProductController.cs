ee49c66b6783d00947b9db3d4cfbc944202a863d
Francisco Rafael <francisco_rafael@hotmail.com.br>
2021-03-04 22:16:19 -0300

    Lendo os produtos

--- Shop/Controllers/ProductController.cs
+++ Shop/Controllers/ProductController.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -16,6 +17,28 @@ namespace Shop.Controllers
         public async Task<ActionResult<List<Product>>> Get([FromServices] DataContext context)
         {
             var products = await context.Products.Include(x => x.Category).AsNoTracking().ToListAsync();
+            return products;
+        }
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public async Task<ActionResult<Product>> GetById([FromServices] DataContext context, int id)
+        {
+            //select pelo o id do produto
+            var product = await context.Products.Include(x => x.Category).AsNoTracking().FirstOrDefaultAsync(x => x.Id == id);
+            return product;
+        }
+
+        [HttpGet] //product/categories/1
+        [Route("categories/{id:int}")]
+        public async Task<ActionResult<List<Product>>> GetByCategory([FromServices] DataContext context, int id)
+        {
+            var products = await context
+            .Products
+            .Include(x => x.Category)
+            .AsNoTracking()
+            .Where(x => x.Category.Id == id)
+            .ToListAsync(); //ToList sempre no final
             return products;
         }
     }
