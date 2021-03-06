e523f9816b6d263806b817f43c9d21c549602677
Francisco Rafael <francisco_rafael@hotmail.com.br>
2021-03-06 17:26:13 -0300

    Autenticacao e Autorizacao

--- Shop/Startup.cs
+++ Shop/Startup.cs
@@ -1,9 +1,12 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
 using Shop.Data;
 
 namespace Shop
@@ -21,6 +24,26 @@ namespace Shop
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            //gera um chave simetrica, transforma essa chave para um formato de bats
+            var key = Encoding.ASCII.GetBytes(Settings.Secret);
+            services.AddAuthentication(x =>
+           {
+               x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+               x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+           })
+           .AddJwtBearer(x =>
+            {
+                x.RequireHttpsMetadata = false;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            });
 
             //Tudo pronto para os Controllers funcionarem
             //services.AddDbContext<DataContext>(opt => opt.UseInMemoryDatabase("Database")); //tipo de banco , informando a aplicao que temos DbContext
@@ -41,6 +64,8 @@ namespace Shop
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseAuthentication();
 
             app.UseAuthorization();
 
