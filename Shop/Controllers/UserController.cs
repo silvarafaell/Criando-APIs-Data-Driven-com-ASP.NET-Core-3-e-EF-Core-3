046221ecd7e3c2cb2d0e335d57dcc137ee7f77a8
Francisco Rafael <francisco_rafael@hotmail.com.br>
2021-03-07 15:27:12 -0300

    Autenticando

---
+++ Shop/Controllers/UserController.cs
@@ -0,0 +1,66 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Shop.Data;
+using Shop.Models;
+using System;
+using Microsoft.AspNetCore.Authorization;
+using System.Linq;
+using Shop.Services;
+
+namespace Shop.Controllers
+{
+    [Route("users")]
+    public class UserController : Controller
+    {
+        [HttpPost]
+        [Route("")]
+        [AllowAnonymous]
+        // [Authorize(Roles = "manager")]
+        public async Task<ActionResult<dynamic>> Post([FromServices] DataContext context, [FromBody] User model)
+        {
+            // Verifica se os dados são válidos
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            try
+            {
+                context.Users.Add(model);
+                await context.SaveChangesAsync();
+
+                // Esconde a senha
+                model.Password = "";
+                return model;
+            }
+            catch (Exception)
+            {
+                return BadRequest(new { message = "Não foi possível criar o usuário" });
+
+            }
+        }
+
+        [HttpPost]
+        [Route("login")]
+        [AllowAnonymous]
+        public async Task<ActionResult<dynamic>> Authenticate([FromServices] DataContext context, [FromBody] User model)
+        {
+            var user = await context.Users
+                .AsNoTracking()
+                .Where(x => x.Username == model.Username && x.Password == model.Password)
+                .FirstOrDefaultAsync();
+
+            if (user == null)
+                return NotFound(new { message = "Usuário ou senha inválidos" });
+
+            var token = TokenService.GenerateToken(user);
+            // Esconde a senha
+            user.Password = "";
+            return new
+            {
+                user = user,
+                token = token
+            };
+        }
+    }
+}
